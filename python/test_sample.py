effbde49c43f34b43e457d8ff594600ffc6537c8
Jakub Liput <jakub.liput@gmail.com>
2016-05-26 23:28:07 +0200

    Python's with block support for Selenium WD, with screenshot taking on exception

--- python/test_sample.py
+++ python/test_sample.py
@@ -3,6 +3,19 @@ from selenium.webdriver.common.keys import Keys
 from xvfbwrapper import Xvfb
 import pyscreenshot as ImageGrab
 
+# monkey-patching Selenium WebDriver
+
+def wd_enter(wd):
+    return wd
+
+def wd_exit(wd, exc_type, exc_val, exc_tb):
+    print 'An error occured in test -> error.png'
+    ImageGrab.grab_to_file('error.png')
+    wd.close()
+
+webdriver.remote.webdriver.WebDriver.__enter__ = wd_enter
+webdriver.remote.webdriver.WebDriver.__exit__ = wd_exit
+
 # Screenshots:
 # ---
 # fullscreen
@@ -20,16 +33,17 @@ def test_hello():
     driver = webdriver.Firefox()
     # driver = webdriver.Chrome()
     # driver = webdriver.Opera({"browserName":"opera","chromeOptions":{"args":[],"extensions":[],"binary":"/usr/bin/opera"}})
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
-    elem.send_keys("pycon")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
-    # to file
-    ImageGrab.grab_to_file('im.png')
-    driver.close()
+    with driver:
+        driver.get("http://www.python.org")
+        assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        # raise 'bad'
+        # to file
+        ImageGrab.grab_to_file('end.png')
 
 
-with Xvfb(width=1280, height=1024, colordepth=24) as xvfb:
-    test_hello()
+# with Xvfb(width=1280, height=1024, colordepth=24) as xvfb:
+test_hello()
